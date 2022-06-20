d404fc9619566a57d4b02056afed75b2fe5da20f
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2022-06-20 11:51:31 +0500

    fixed unit tests

--- src/main/java/com/maslov/homeworkboot/service/impl/QuestionListImpl.java
+++ src/main/java/com/maslov/homeworkboot/service/impl/QuestionListImpl.java
@@ -4,7 +4,6 @@ import com.maslov.homeworkboot.dao.impl.DataHandlerImpl;
 import com.maslov.homeworkboot.domain.Quiz;
 import com.maslov.homeworkboot.service.QuestionList;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedList;
@@ -13,8 +12,12 @@ import java.util.List;
 @Service
 @Slf4j
 public class QuestionListImpl implements QuestionList {
-    @Autowired
-    private DataHandlerImpl handler;
+
+    private final DataHandlerImpl handler;
+
+    public QuestionListImpl(DataHandlerImpl handler) {
+        this.handler = handler;
+    }
 
     @Override
     public List<Quiz> getQuiz() {
@@ -22,7 +25,7 @@ public class QuestionListImpl implements QuestionList {
         List<Quiz> queue = new LinkedList<>();
         try {
             List<String[]> dataGetter = handler.getResourceAsStream();
-            for (var el: dataGetter) {
+            for (var el : dataGetter) {
                 queue.add(new Quiz(
                         Long.parseLong(el[0]), el[1], el[2], el[3], el[4], Integer.parseInt(el[5].trim())));
             }
